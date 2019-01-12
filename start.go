514245230b6edf048bc83392f2d86c8fb8a60455
kikuchi kakeru <kkikuchi1539@gmail.com>
2019-01-12 20:58:29 +0900

    :sparkles: Named return values

--- start.go
+++ start.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
