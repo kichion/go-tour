867b1ded27542a75bc02e71fdf3a7d31ebe6efa7
kikuchi kakeru <kkikuchi1539@gmail.com>
2019-01-13 20:57:03 +0900

    :sparles: Variables with initializers

--- start.go
+++ start.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
