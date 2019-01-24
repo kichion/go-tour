f97b6d671ca6c2efe4b8623afc723c256c51bf9a
kikuchi kakeru <kkikuchi1539@gmail.com>
2019-01-24 13:07:48 +0900

    :sparkles: Exercise: Equivalent Binary Trees

--- start.go
+++ start.go
@@ -5,22 +5,32 @@ import "golang.org/x/tour/tree"
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	var walkSub func(t *tree.Tree)
-	walkSub = func(t *tree.Tree) {
+	var walk_sub func(t *tree.Tree)
+	walk_sub = func(t *tree.Tree) {
         if t != nil {
-            walkSub(t.Left)
+            walk_sub(t.Left)
             ch <- t.Value
-            walkSub(t.Right)
+            walk_sub(t.Right)
         }
     }
-	walkSub(t)
+	walk_sub(t)
 	close(ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return true
+	treeCh1 := make(chan int)
+    treeCh2 := make(chan int)
+    go Walk(t1, treeCh1)
+    go Walk(t2, treeCh2)
+    for v1 := range treeCh1 {
+        v2, ok := <- treeCh2
+        if !ok || v1 != v2 {
+            return false
+        }
+    }
+    return true
 }
 
 func main() {
@@ -29,4 +39,6 @@ func main() {
 	for v := range ch {
         println(v)
     }
+	println(Same(tree.New(1), tree.New(1)))
+	println(Same(tree.New(1), tree.New(2)))
 }
