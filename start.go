ddcdee581a868cc9896b7f676555b74529aef20b
kikuchi kakeru <kkikuchi1539@gmail.com>
2019-01-24 10:40:26 +0900

    :sparkles: Exercise: Images

--- start.go
+++ start.go
@@ -1,17 +1,27 @@
 package main
 
-import "golang.org/x/tour/reader"
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
 
-type MyReader struct{}
+type Image struct{}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
-func (a MyReader) Read(rb []byte) (l int, e error) {
-	for l, e = 0, nil; l < len(rb); l++ {
-		rb[l] = 'A'
-	}
-	return
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 256, 256)
+}
+
+func (img Image) ColorModel() image.ColorModel {
+	return color.RGBAModel
+}
+
+func (img Image) At(x, y int) color.Color {
+	v := uint8(x^y)
+	return color.RGBA{v, v, 128, 189}
 }
 
 func main() {
-	reader.Validate(MyReader{})
+	m := Image{}
+	pic.ShowImage(m)
 }
